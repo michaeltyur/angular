588e4dc16fdd8a8ab36359f00bc0207775eaf50f
michael <michaeltiour@gmail.com>
2018-09-26 19:42:52 +0300

    ewewe

--- src/app/pipes/tomark.pipe.ts
+++ src/app/pipes/tomark.pipe.ts
@@ -1,4 +1,5 @@
 import{Pipe, PipeTransform,Component} from '@angular/core'
+import {BrowserModule, DomSanitizer} from '@angular/platform-browser'
 
 @Pipe({
     name:'tomark'
@@ -6,17 +7,21 @@ import{Pipe, PipeTransform,Component} from '@angular/core'
 
 export class TomarkPipe implements PipeTransform
 {
-    transform(recipe,value){
-
-       // let name=value.replace(/^\w/, c => c.toUpperCase());
-
-        if( recipe && value )
-        {
-            var highlighted = recipe.replace(/${value}/g, `<span class="red">${value}</span>`);
-            return highlighted;
-
+    constructor(private sanitizer: DomSanitizer){}
+    transform(value: any, args: any): any {
+        if (!args) {
+          return value;
         }
-        else return recipe;
-        
-    }
+        // Match in a case insensitive maneer
+        const re = new RegExp(args, 'gi');
+        const match = value.match(re);
+    
+        // If there's no match, just return the original value.
+        if (!match) {
+          return value;
+        }
+    
+         value = value.replace(re, "<mark style='background:#ffc107;padding:0;'>" + match[0] + "</mark>");
+         return this.sanitizer.bypassSecurityTrustHtml(value);
+      }
 }
